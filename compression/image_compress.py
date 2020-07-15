c3d4ae58a3411cffbf21ba4efe479d535532f519
Ben Smus <bensmus@gmail.com>
2020-07-14 17:24:15 -0700

    Works with png now

--- compression/image_compress.py
+++ compression/image_compress.py
@@ -1,23 +1,24 @@
 from PIL import Image
 
 # only compress images that are bigger than 100 KB
-# first get it working with jpg 
-# as it seems like the most common format
 
-# this image has a size of 937 KB 
-name = r'C:\Users\Ben Smus\Evident_Point\FTP\Carnegie_Clone\ftpfiles_original_TEST_WITH_EPUBS\american_promise\OEBPS\images\ROA_04249_EM_M03.jpg'
-
-
-def compress(name, quality):
-    '''
-    Warning: Overwrites the image! 
-    Will be copying from the ftp server anyways, so there is a backup.
-    Quality is from 0 -> 100, 100 being original qualty.
-    '''
-
-    img = Image.open(name)
-    img.save(name, optimize=True, quality=quality)
+# this image has a size of 334 KB 
+inname = 'testimg\\original_phone.png'
+outname = 'testimg\\copy_phone.jpg'
 
 
-# wow, it compressed it to only 283 KB, was not expecting that
-compress(name, 90)
+def compress(inname, outname, quality):
+    img = Image.open(inname)
+
+    if inname[-3:] != 'jpg':  # convert to jpg because that is the most compact format
+        jpgimg = img.convert('RGB')
+        print(1)
+    else:
+        jpgimg = img
+
+    jpgimg.save(outname, optimize=True, quality=quality)
+
+
+# compress 100 with optimize does not do anything (makes sense)
+# compress 90 reduces file size to 140 KB
+compress(inname, outname, 90)  # side by side comparison reveals no perceptible change!
