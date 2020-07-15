bdd96e7042556d89fd6df9e58a756bb63744cbbc
Ben Smus <bensmus@gmail.com>
2020-07-14 18:24:50 -0700

    turns everything into a jpg

--- compression/image_compress.py
+++ compression/image_compress.py
@@ -1,24 +1,38 @@
 from PIL import Image
-
-# only compress images that are bigger than 100 KB
+import os 
 
 # this image has a size of 334 KB 
-inname = 'testimg\\original_phone.png'
-outname = 'testimg\\copy_phone.jpg'
+name = r'C:\Users\Ben Smus\Evident_Point\da-epub-media-compression\compression\testimg\phone.png'
 
 
-def compress(inname, outname, quality):
-    img = Image.open(inname)
+def split_ext(name):
+    dotindex = name.find('.')
+    return name[:dotindex], name[dotindex:]  # asdf, .jpg
 
-    if inname[-3:] != 'jpg':  # convert to jpg because that is the most compact format
-        jpgimg = img.convert('RGB')
-        print(1)
+
+def compress(name, quality):
+    pre, ext = split_ext(name)
+    
+    if ext != '.jpg':  # convert to jpg because that is the most compact format
+
+        print('converting to jpg...')
+        outname = pre + '.jpg'
+        
+        with Image.open(name) as img:
+            jpgimg = img.convert('RGB')
+
+        # need to explicitly delete uncompressed file
+        os.remove(name)
+        
     else:
+        img = Image.open(name)
         jpgimg = img
+        outname = name
 
+    # don't forget that we can have 4 character file extensions
     jpgimg.save(outname, optimize=True, quality=quality)
 
 
 # compress 100 with optimize does not do anything (makes sense)
 # compress 90 reduces file size to 140 KB
-compress(inname, outname, 90)  # side by side comparison reveals no perceptible change!
+compress(name, 90)  # side by side comparison reveals no perceptible change!
